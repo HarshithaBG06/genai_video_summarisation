63c609168014cadf3018c53259a049b6751670b9
HarshithaBG06 <163632796+HarshithaBG06@users.noreply.github.com>
2025-04-23 12:09:00 +0530

    Update genAI_Video_Summary_Final.py

--- genAI_Video_Summary_Final.py
+++ genAI_Video_Summary_Final.py
@@ -91,7 +91,7 @@ if youtube_url:
       summary = describe_video()
       st.session_state["summary"]=summary
 
-  st.markdown("## video summary:")
+  st.markdown("### video summary:")
   st.markdown(summary)
 
 
